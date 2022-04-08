686897fff546e63d183a21d8f0165b983420df58
Ersan Karimi <ersankarimii311@gmail.com>
2022-04-08 21:46:46 +0800

    Update : memperbarui Header.jsx
    - Mengimpor komponen DoubleArrow
    - Mengembalikan sebuah komponen header

--- src/features/ui/header/header.jsx
+++ src/features/ui/header/header.jsx
@@ -1,1 +1,15 @@
-import React from "react";
+import React from 'react'
+import DoubleArrow from './components/arrow/DoubleArrow'
+
+const Header = () => {
+    return (
+        <header
+            className={
+                'sticky top-0 flex min-w-full items-center bg-gradient-to-b from-main-bg-dark-1 to-main-bg-dark-1 p-4 text-white backdrop-blur-sm'
+            }>
+            <DoubleArrow />
+        </header>
+    )
+}
+
+export default Header
