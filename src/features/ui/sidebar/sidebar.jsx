1970365a98eee8f44bfbfc67ba6daae9e03960b0
Ersan Karimi <ersankarimii311@gmail.com>
2022-04-09 22:15:58 +0800

    Update : memperbarui Sidebar.jsx

--- src/features/ui/sidebar/sidebar.jsx
+++ src/features/ui/sidebar/sidebar.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import './Sidebar.style.css'
+import { Dropdown } from './components'
 
 const Sidebar = () => {
-    return <h1>asdas</h1>
+    return (
+        <aside className={'sidebar'}>
+            <div className="sidebar__nav">
+                <h1 className={'sidebar__header'}>Explora Web Tools</h1>
+
+                <Dropdown />
+            </div>
+            <div className="sidebar__overlay min-w-[50%]"></div>
+        </aside>
+    )
 }
 
 export default Sidebar
