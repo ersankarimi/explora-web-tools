f2718c923cc313e7a0a919ae1123558a0951a88c
Ersan Karimi <ersankarimii311@gmail.com>
2022-04-09 22:14:47 +0800

    Update : memperbarui App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Home } from '@pages'
 
 const App = () => {
     return (
-        <div className="container min-h-screen min-w-full max-w-full bg-gradient-to-b from-main-bg-dark-1 to-main-bg-dark-2">
+        <div className="container flex min-h-screen min-w-full max-w-full overflow-x-hidden bg-gradient-to-b from-main-bg-dark-1 to-main-bg-dark-2">
             <Routes>
                 <Route path={'/'} element={<Home />}></Route>
             </Routes>
