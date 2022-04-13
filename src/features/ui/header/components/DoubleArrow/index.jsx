4f77c57b7daef59f2934c4c1536e675ceb701538
Ersan Karimi <ersankarimii311@gmail.com>
2022-04-13 16:29:38 +0800

    Update : memperbarui index.jsx

---
+++ src/features/ui/header/components/DoubleArrow/index.jsx
@@ -0,0 +1,3 @@
+import DoubleArrow from './DoubleArrow'
+
+export { DoubleArrow }
--- src/features/ui/sidebar/components/index.jsx
+++ src/features/ui/sidebar/components/index.jsx
@@ -1,2 +1,2 @@
-export * from './dropdown'
-export * from './arrow'
+export * from './Dropdown'
+export * from './ArrowSidebar'
