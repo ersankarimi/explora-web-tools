167649846fbb72b32a52a74bb0058507658a838d
Ersan Karimi <ersankarimii311@gmail.com>
2022-04-13 16:31:44 +0800

    Update : memperbarui class Header.jsx dan memindahkannya didadlam file Header.style.css

--- src/features/ui/header/Header.jsx
+++ src/features/ui/header/Header.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import DoubleArrow from './components/arrow/DoubleArrow'
+import { DoubleArrow } from './components'
+import './Header.style.css'
 
 const Header = () => {
     return (
-        <header
-            className={
-                'sticky top-0 flex min-w-full items-center bg-gradient-to-b from-main-bg-dark-1 to-main-bg-dark-1/50 p-4 text-white backdrop-blur-sm'
-            }>
+        <header className={'header'}>
             <DoubleArrow />
         </header>
     )
