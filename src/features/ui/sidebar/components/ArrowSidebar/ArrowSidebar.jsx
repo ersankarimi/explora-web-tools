d7ef018d5795a62753ae11c9fba0150fedbebed9
Ersan Karimi <ersankarimii311@gmail.com>
2022-04-13 16:36:13 +0800

    Update : memperbarui ArrowSidebar.jsx

---
+++ src/features/ui/sidebar/components/ArrowSidebar/ArrowSidebar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+const ArrowSidebar = ({ isOpen }) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}>
+            <motion.path
+                animate={{ rotate: isOpen ? 90 : 0 }}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9 5l7 7-7 7"
+            />
+        </svg>
+    )
+}
+
+export default ArrowSidebar
