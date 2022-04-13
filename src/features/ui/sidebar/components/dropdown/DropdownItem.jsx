96aa8793215416c5bf02967fef207be195e95c39
Ersan Karimi <ersankarimii311@gmail.com>
2022-04-13 16:38:41 +0800

    Update : Memperbarui  DropdownItem.jsx
    - Mengimpor beberapa modul
    - Membuat function handleDropdownOpen() yang akan menjalankan perintah setDropdownIsOpen state
    - Membuat function handleCloseSidebar yang akan menjalankan perintah toggleSidebar() jika width layar <= 639

--- src/features/ui/sidebar/components/dropdown/DropdownItem.jsx
+++ src/features/ui/sidebar/components/dropdown/DropdownItem.jsx
@@ -1,20 +1,68 @@
-import React from 'react'
-import ButtonDropdown from './ButtonDropdown'
+import React, { useState } from 'react'
+import { motion } from 'framer-motion'
+import { NavLink } from 'react-router-dom'
+import { useSidebar } from '@hooks'
 import './DropdownItem.style.css'
+import ButtonDropdown from './ButtonDropdown'
+import { ArrowSidebar } from '../ArrowSidebar'
 
-const DropdownItem = ({ name, item }) => {
+const DropdownItem = ({ name, item, path }) => {
+    const [dropdownIsOpen, setDropdownIsOpen] = useState(false)
+    const [toggleSidebar] = useSidebar()
+
+    const handleDropdownOpen = () => {
+        setDropdownIsOpen((condition) => !condition)
+    }
+
+    // * This function will handle close and open the sidebar when click the link in dropdown
+    const handleCloseSidebar = () => {
+        const { width } = window.screen
+        width <= 639 && toggleSidebar()
+    }
+
     return (
         <li className={'sidebar__dropdown-item'}>
-            {/* button dropdown dalam sini */}
-            <ButtonDropdown name={name} />
-            {/* dropdown item */}
-            <ul className={'sidebar__dropdown-collapse'}>
+            <ButtonDropdown>
+                <button className={'sidebar__dropdown-button'} onClick={handleDropdownOpen}>
+                    {name}
+                    <span>
+                        <ArrowSidebar isOpen={dropdownIsOpen} />
+                    </span>
+                </button>
+            </ButtonDropdown>
+            <motion.ul
+                className={'sidebar__dropdown-collapse'}
+                animate={{
+                    display: dropdownIsOpen ? 'block' : 'none'
+                }}
+                transition={{
+                    duration: 0.5
+                }}>
                 {item.map((el, i) => (
-                    <li className={'sidebar__dropdown-collapse-item'} key={i}>
-                        {el}
-                    </li>
+                    <motion.li
+                        className={'sidebar__dropdown-collapse-item'}
+                        key={i}
+                        animate={{
+                            opacity: dropdownIsOpen ? 1 : 0
+                        }}
+                        transition={{
+                            delay: i / 90
+                        }}>
+                        <NavLink
+                            to={`${path}/${el.split(' ').join('').toLowerCase()}`}
+                            style={({ isActive }) => {
+                                return {
+                                    color: isActive
+                                        ? 'rgba(250,250,250,.80)'
+                                        : 'rgba(250,250,250,.60)'
+                                }
+                            }}
+                            onClick={handleCloseSidebar}>
+                            {el}
+                        </NavLink>
+                    </motion.li>
                 ))}
-            </ul>
+            </motion.ul>
         </li>
     )
 }
