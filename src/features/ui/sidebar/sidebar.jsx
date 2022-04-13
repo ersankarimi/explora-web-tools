e8e92fcd118103c13a628566ccd2efa5b880d38d
Ersan Karimi <ersankarimii311@gmail.com>
2022-04-13 16:35:43 +0800

    Update : memperbarui sidebar.jsx
    - Inisialisasi custom hook useSidebar()
    - Membuat function baru handleShowHiddenSidebar jika ukuran lebar layar <= 639, sidebar terbuka, dan target click tidak emmiliki class sidebar__dropdown-button maka akan menjalankan function toggleSidebar
    - Memberi animasi motion
    - Memberikan evenet onClick

--- src/features/ui/sidebar/sidebar.jsx
+++ src/features/ui/sidebar/sidebar.jsx
@@ -1,18 +1,35 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+import { Link } from 'react-router-dom'
+import { useSidebar } from '@hooks'
 import './Sidebar.style.css'
 import { Dropdown } from './components'
 
 const Sidebar = () => {
+    const [sidebarIsOpen, toggleSidebar] = useSidebar()
+
+    // * This function will handle close and open sidebar when click in window
+    // * when viewport is less than 639, sidebar is opened, and target
+    // * classList not same as sidebar__dropdown-button
+    const handleShowHiddenSidebar = (e) => {
+        const viewport = window.innerWidth
+        viewport <= 639 &&
+            sidebarIsOpen &&
+            !e.target.classList.contains('sidebar__dropdown-button') &&
+            toggleSidebar()
+    }
+
     return (
-        <aside className={'sidebar'}>
-            <div className="sidebar__nav">
-                <h1 className={'sidebar__header'}>Explora Web Tools</h1>
+        <motion.aside className="sidebar">
+            <div className="sidebar__nav" onClick={handleShowHiddenSidebar}>
+                <h1 className="sidebar__header">
+                    <Link to={'/'}>Explora Web Tools</Link>
+                </h1>
 
                 <Dropdown />
             </div>
-            <div className="sidebar__overlay min-w-[50%]"></div>
-        </aside>
+            <div className="sidebar__overlay" onClick={handleShowHiddenSidebar}></div>
+        </motion.aside>
     )
 }
-
 export default Sidebar
