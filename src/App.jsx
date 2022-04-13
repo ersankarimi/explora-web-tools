8ebb78ce2972b5644fda8d2bdcdc824067afe86a
Ersan Karimi <ersankarimii311@gmail.com>
2022-04-13 16:23:43 +0800

    Update : memperbarui App.jsx
    - Menambahkan function function handleWindowResize() untuk mengecek keadaan lebar dari layar pengguna saat ini ketika di resize ukurannya 639px kebawah maka akan otomatis menutup sidebar, ji
    - Membungkun function handleWindowResize() didalam useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 import { Header, Sidebar, Footer } from '@features/ui'
 import { motion } from 'framer-motion'
 import { useSidebar } from '@hooks'
-import './App.styles.css'
+import './App.style.css'
 
 const App = () => {
-    const [sidebarIsOpen] = useSidebar()
+    const [sidebarIsOpen, toggleSidebar] = useSidebar()
+
+    const handleWindowResize = () => {
+        const viewport = window.innerWidth
+        viewport > 640 && !sidebarIsOpen && toggleSidebar()
+        viewport <= 639 && sidebarIsOpen && toggleSidebar()
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', handleWindowResize)
+        return () => {
+            window.removeEventListener('resize', handleWindowResize)
+        }
+    }, [window.innerWidth])
 
     return (
         <>
