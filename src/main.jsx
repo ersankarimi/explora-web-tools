3427ee7da9c80b505d92569f4bfb25234fe0031e
Ersan Karimi <ersankarimii311@gmail.com>
2022-04-13 15:40:25 +0800

    Refactor : refactor main.jsx
    - Membuat semua perutean konten di dalam main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
+import { SidebarContextProvider } from '@context'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import { Home } from '@pages'
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <SidebarContextProvider>
+        <BrowserRouter>
+            <Routes>
+                <Route path={'/'} element={<App />}>
+                    <Route index element={<Home />} />
+                    <Route
+                        path={'css/cssboxshadow'}
+                        element={<h1 className={' text-white'}>box shadow</h1>}
+                    />
+                    <Route
+                        path={'css/relativelengthunits'}
+                        element={<h1 className={' text-white'}>CSS LEngth</h1>}
+                    />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    </SidebarContextProvider>,
     document.getElementById('root')
 )
