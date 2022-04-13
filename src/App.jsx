df8769788d04831fadea43cd2d1ee4ec5d2ae276
Ersan Karimi <ersankarimii311@gmail.com>
2022-04-13 15:38:17 +0800

    Refactor : Refactor App component
    - Membuat App component sebagai titik utama perenderan website dan Outlet nya
    - Memperbarui App component dengan memberinya motion
    - Memperbarui App.style.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import { Home } from '@pages'
+import { Outlet } from 'react-router-dom'
+import { Header, Sidebar, Footer } from '@features/ui'
+import { motion } from 'framer-motion'
+import { useSidebar } from '@hooks'
+import './App.styles.css'
 
 const App = () => {
+    const [sidebarIsOpen] = useSidebar()
+
     return (
-        <div className="container flex min-h-screen min-w-full max-w-full overflow-x-hidden bg-gradient-to-b from-main-bg-dark-1 to-main-bg-dark-2">
-            <Routes>
-                <Route path={'/'} element={<Home />}></Route>
-            </Routes>
-        </div>
+        <>
+            <motion.div
+                className="container__sidebar"
+                animate={{
+                    x: sidebarIsOpen ? 0 : -400,
+                    display: sidebarIsOpen ? 'block' : 'none'
+                }}
+                transition={{
+                    duration: 0.25,
+                    type: 'spring'
+                }}>
+                <Sidebar />
+            </motion.div>
+            <div className="container__content" style={{ minHeight: '100vh' }}>
+                <Header />
+                <div className="container__content-main">
+                    {/* // * Render Outlet */}
+                    <Outlet />
+                    <Outlet />
+                </div>
+                <Footer />
+            </div>
+        </>
     )
 }
 
