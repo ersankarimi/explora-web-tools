8597ee8ebb50397953d2e1f6acd08ae74d7daef7
Ersan Karimi <ersankarimii311@gmail.com>
2022-04-17 14:34:59 +0800

    Update : mengubah nama cardData.jsx menjadi Card.data.jsx

---
+++ src/features/HomePage/components/Card/Card.data.jsx
@@ -0,0 +1,16 @@
+export const cardData = [
+    {
+        header: 'CSS TOOLS',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor',
+        path: 'css',
+        extraClass: 'bg-gradient-to-tr from-[#8E2DE2] to-[#4A00E0]'
+    },
+    {
+        header: 'HTML TOOLS',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor',
+        path: 'html',
+        extraClass: 'bg-gradient-to-tr from-[#f46b45] to-[#eea849]'
+    }
+]
