687e34e928e7229f56874a508cc3413a35edf89c
Ersan Karimi <ersankarimii311@gmail.com>
2022-04-17 14:49:10 +0800

    Update : memperbarui Sidebar.jsx
    - Menambahkan beberapa komentar
    - Mengganti variabel viewport menjadi innerWidth (hasil destructuring object window)

--- src/features/ui/sidebar/sidebar.jsx
+++ src/features/ui/sidebar/sidebar.jsx
@@ -1,23 +1,57 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { motion } from 'framer-motion'
 import { Link } from 'react-router-dom'
 import { useSidebar } from '@hooks'
 import './Sidebar.style.css'
 import { Dropdown } from './components'
 
+/**
+ * * Returns a Sidebar component.
+ * @returns {any} of the Sidebar component.
+ */
 const Sidebar = () => {
+    /**
+     * @constant
+     * @type {boolean} represents the state of the sidebar
+     * @default true when the viewport is greater than 639px
+     *
+     * @constant
+     * @type {function} represents the function to toggle the sidebar
+     * @default useSidebar(SidebarContext)
+     */
     const [sidebarIsOpen, toggleSidebar] = useSidebar()
 
-    // * This function will handle close and open sidebar when click in window
-    // * when viewport is less than 639, sidebar is opened, and target
-    // * classList not same as sidebar__dropdown-button
+    /**
+     * * Returns a toggleSedebar function,
+     * * if the innerWidth is <= 639px and,
+     * *  target event doesn't have the className 'sidebar__dropdown-button' and 'arrow-button4.
+     * @param {e} e - The event object.
+     * @type {number} innerWidth - The innerWidth of the window.
+     * @type {boolean} sidebarIsOpen - The state of the sidebar.W
+     * @returns {any} of the toggleSidebar() function if the innerWidth <= 639.
+     */
     const handleShowHiddenSidebar = (e) => {
-        const viewport = window.innerWidth
-        viewport <= 639 &&
+        const { innerWidth } = window
+        innerWidth <= 639 &&
             sidebarIsOpen &&
             !e.target.classList.contains('sidebar__dropdown-button') &&
+            !e.target.classList.contains('arrow-dropdown') &&
             toggleSidebar()
     }
+
+    /**
+     * * Set and remove a class for body,
+     * * if the sidebar is open and innerWidth <= 639, the body will have a class "overflow-hidden".
+     * @type {any} of the useEffect() function.
+     * @type {number} innerWidth - The innerWidth of the window.
+     * @type {boolean} sidebarIsOpen - The state of the sidebar.
+     */
+    useEffect(() => {
+        const { innerWidth } = window
+        sidebarIsOpen && innerWidth <= 639
+            ? document.body.classList.add('overflow-hidden')
+            : document.body.classList.remove('overflow-hidden')
+    }, [sidebarIsOpen])
 
     return (
         <motion.aside className="sidebar">
