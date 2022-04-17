5f802f1e118f4f0a108c80276c0292b3c4646d17
Ersan Karimi <ersankarimii311@gmail.com>
2022-04-17 14:47:20 +0800

    Update : memperbarui Navbar.jsx dan Navbar.style.css
    - Pada Navabar.jsx, menambahkan beberapa komentar
    - Pada Navbar.style.css, menambahkan class lg:py-4 dan lg:px-10

--- src/features/ui/Navbar/Navbar.jsx
+++ src/features/ui/Navbar/Navbar.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import { DoubleArrow } from './components'
 import './Navbar.style.css'
 
+/**
+ * * Returns a Navbar component.
+ * @returns {any} of the Navbar components.
+ */
+
 const Navbar = () => {
     return (
         <header className={'navbar'}>
