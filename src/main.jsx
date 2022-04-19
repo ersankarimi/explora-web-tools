a35e419f141232b6995c15d72cd44601e5182fd1
Ersan Karimi <ersankarimii311@gmail.com>
2022-04-19 10:38:07 +0800

    Update : memperbarui main.jsx
    - Import modul baru yaitu MenuSection dan DataContentContextProvider
    
    - Menambah pembungkus dari BrowserRouter yaitu <DataContentContextProvider/>
    
    - Mengganti Route path css/relativelengthunits menjadi :sectionPath. Yang mana ini akan menentukan path dari user /css atau /html dan akan merender bagian yang sesuai

--- src/main.jsx
+++ src/main.jsx
@@ -4,25 +4,25 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import { SidebarContextProvider } from '@context'
 import App from './App'
-import { Home } from '@pages'
+import { Home, MenuSection } from '@pages'
+import { DataContentContextProvider } from './context'
 
 ReactDOM.render(
     <SidebarContextProvider>
-        <BrowserRouter>
-            <Routes>
-                <Route path={'/'} element={<App />}>
-                    <Route index element={<Home />} />
-                    <Route
-                        path={'css/cssboxshadow'}
-                        element={<h1 className={' text-white'}>box shadow</h1>}
-                    />
-                    <Route
-                        path={'css/relativelengthunits'}
-                        element={<h1 className={' text-white'}>CSS LEngth</h1>}
-                    />
-                </Route>
-            </Routes>
-        </BrowserRouter>
+        <DataContentContextProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path={'/'} element={<App />}>
+                        <Route index element={<Home />} />
+                        <Route
+                            path={'css/cssboxshadow'}
+                            element={<h1 className={' text-white'}>box shadow</h1>}
+                        />
+                        <Route path={':sectionPath/'} element={<MenuSection />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </DataContentContextProvider>
     </SidebarContextProvider>,
     document.getElementById('root')
 )
