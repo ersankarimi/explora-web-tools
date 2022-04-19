32e4968e6414ba6d8e82015b837bdba1773067aa
Ersan Karimi <ersankarimii311@gmail.com>
2022-04-19 10:58:54 +0800

    Make new file : membuat file root baru yaitu MenuSection.jsx

---
+++ src/pages/routes/MenuSection/MenuSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Menu } from '@features/Menu'
+
+/**
+ ** Returns a MenuSection component.
+ * @returns {JSX.Element} Menu - JSX element of the MenuSection component.
+ */
+const MenuSection = () => {
+    return <Menu />
+}
+
+export default MenuSection
