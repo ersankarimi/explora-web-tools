40fe66386abbf401196321deb493a54fa32c4e22
Ersan Karimi <ersankarimii311@gmail.com>
2022-04-19 14:35:48 +0800

    Update : memperbarui komponen UnderConstruction.jsx

--- src/features/ui/UnderConstructions/UnderConstructions.jsx
+++ src/features/ui/UnderConstructions/UnderConstructions.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
-import './UnderConstruction.style.css'
+import './UnderConstructions.style.css'
+import { AlertIcon } from './components/'
 
-const UnderConstructions = () => {}
+const UnderConstructions = () => {
+    return (
+        <div className="under-construction">
+            <div className="under-construction_icon">
+                <AlertIcon />
+            </div>
+            <div className="under-construction_message">
+                <h2>This site is still under construction</h2>
+            </div>
+        </div>
+    )
+}
 
 export default UnderConstructions
