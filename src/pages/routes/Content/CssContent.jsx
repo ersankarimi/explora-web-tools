e2576af1256a8e5f71ad53638d9b9489f571e558
Ersan Karimi <ersankarimii311@gmail.com>
2022-04-25 16:24:41 +0800

    Make new file : membuat file CssContent
    - File ini akan menjadi routes dari bagian menu css
    -  Menambahkan komentar

---
+++ src/pages/routes/Content/CssContent.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { RelativeLengthUnits } from '@features/Content'
+import { UnderConstructions } from '@features/ui'
+
+/**
+ * * Returns a component that renders the content of the section.
+ * @returns {JSX.Element} CssContent - JSX Element of the CSS Content Routes .
+ */
+const CssContent = () => {
+    /**
+     * @constant
+     * @type {string} section - the current section pathname from the url params object
+     */
+    const { section } = useParams()
+
+    switch (section) {
+        case 'relativelengthunits':
+            return <RelativeLengthUnits />
+
+        default:
+            return <UnderConstructions />
+    }
+}
+
+export default CssContent
