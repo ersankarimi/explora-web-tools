5bf2992911d4342cb76143b3d3ed903695362cd9
Ersan Karimi <ersankarimii311@gmail.com>
2022-04-28 20:45:34 +0800

    Make : membuat komponen baru DropdownArror.jsx

---
+++ src/features/ui/sidebar/components/DropdownArrow/DropdownArrow.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+import arrow from './arrow.svg'
+
+/**
+ * * Returns a DoubleArrow component
+ * @param {isOpen} isOpen - represents the state of the sidebar
+ * @returns {any} of the DoubleArrow component
+ */
+const DropdownArrow = ({ isOpen }) => {
+    return (
+        <motion.img
+            className="arrow-dropdown h-6 w-6 cursor-pointer"
+            src={arrow}
+            alt="Dropdown Arrow Icon"
+            animate={{ rotate: isOpen ? 90 : 0 }}
+        />
+    )
+}
+
+export default DropdownArrow
