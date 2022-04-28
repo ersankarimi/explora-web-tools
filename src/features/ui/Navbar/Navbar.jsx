46c4a41d02cca915cf05d0857034347fe357d09a
Ersan Karimi <ersankarimii311@gmail.com>
2022-04-28 20:44:19 +0800

    Update : memperbarui Navbar.jsx
    - Import SidebarContext dari "@context" dan useSidebar dari "@hooks".
    - Inisialisasi custom hooks useSidebar()
    - Update classList style komponen tersebut
    - Inisialisasi variants motion pada tag img

--- src/features/ui/Navbar/Navbar.jsx
+++ src/features/ui/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import { DoubleArrow } from './components'
-import './Navbar.style.css'
+import { motion } from 'framer-motion'
+import { SidebarContext } from '@context'
+import { useSidebar } from '@hooks'
+import doubleArrow from './doubleArrow.svg'
 
 /**
  * * Returns a Navbar component.
@@ -8,9 +10,48 @@ import './Navbar.style.css'
  */
 
 const Navbar = () => {
+    /**
+     * @constant
+     * @type {boolean} represents the state of the sidebar
+     * @default true when the viewport is greater than 639px
+     *
+     * @constant
+     * @type {function} represents the function to toggle the sidebar
+     */
+    const [sidebarIsOpen, toggleSidebar] = useSidebar()
+
+    /**
+     * @constant
+     * @type {object} represents the motion object
+     * @default motion object
+     */
+    // variants motion for double arrow
+    const variants = {
+        notOpen: {
+            x: [10, -50, 0],
+            rotate: 180
+        },
+        open: {
+            x: [-10, 50, 0],
+            rotate: 360
+        }
+    }
+
     return (
-        <header className={'navbar'}>
-            <DoubleArrow />
+        <header className="sticky top-0 z-30 flex min-w-full items-center bg-gradient-to-b from-main-bg-dark-1 to-main-bg-dark-1/50 p-4 py-4 text-white shadow-header-shadow backdrop-blur-sm lg:px-8">
+            <motion.img
+                variants={variants}
+                animate={sidebarIsOpen ? 'open' : 'notOpen'}
+                transition={{
+                    duration: 0.5,
+                    type: 'spring',
+                    stiffness: 50
+                }}
+                src={doubleArrow}
+                className="h-6 w-6 cursor-pointer md:h-8 md:w-8 xl:h-8 xl:w-16"
+                alt="logo"
+                onClick={toggleSidebar}
+            />
         </header>
     )
 }
