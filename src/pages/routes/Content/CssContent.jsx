37badff6646ebc030211adbbc1a330a8b3d5ec7d
Ersan Karimi <ersankarimii311@gmail.com>
2022-04-28 20:34:59 +0800

    Add : menambahkan komponen baru

--- src/pages/routes/Content/CssContent.jsx
+++ src/pages/routes/Content/CssContent.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import { RelativeLengthUnits } from '@features/Content'
 import { UnderConstructions } from '@features/ui'
 
 /**
