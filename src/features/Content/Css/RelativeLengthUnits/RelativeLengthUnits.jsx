edeeef38013dc2761cf42d5d3bc2b3aa9c1bfcdd
Ersan Karimi <ersankarimii311@gmail.com>
2022-05-14 14:45:52 +0800

    Update : memperbarui RelativeLengthUnits.jsx
    - Memperbarui return value. Komponen ini akan mengembalikan h1 dan komponen <InputOutputForm>

--- src/features/Content/Css/RelativeLengthUnits/RelativeLengthUnits.jsx
+++ src/features/Content/Css/RelativeLengthUnits/RelativeLengthUnits.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { InputOutputForm, Dropdown } from './components'
 
 /**
  * * Returns a component that renders the content of the Relative Length Units content.
  * @returns {JSX.Element} RelativeLengthUnits - JSX Element of the Relative Length Units Content Routes .
  */
 const RelativeLengthUnits = () => {
-    return <h1>asdasdasd</h1>
+    return (
+        <>
+            <h1 className="mt-4 mb-8 text-center font-poppins text-2xl font-extrabold tracking-wide text-white-100 lg:text-4xl">
+                Relative Length Units
+            </h1>
+            <InputOutputForm />
+            {/* <Dropdown /> */}
+        </>
+    )
 }
 
 export default RelativeLengthUnits
