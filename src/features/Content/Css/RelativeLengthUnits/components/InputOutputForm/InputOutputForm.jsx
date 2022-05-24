96367d02e4767b85e5d07e72e156eb33d4706531
Ersan Karimi <ersankarimii311@gmail.com>
2022-05-24 13:36:22 +0800

    Rafactor : refactor RelativeLengthUnits and InputOutputForm components
    - Menambahkan props baru pada komponen InputOutputForm
    - Menghapus beberapa hook pada komponen InputOutputForm dan memindahkan nya ke komponen RelativeLengthUnits
    - Menambahkan beberapa hook pada komponen RelativeLengthUnits dan akan diteruskan sebagai props pada InputOutputForm komponen untuk mendapatkan value pada komponen tersebut

--- src/features/Content/Css/RelativeLengthUnits/components/InputOutputForm/InputOutputForm.jsx
+++ src/features/Content/Css/RelativeLengthUnits/components/InputOutputForm/InputOutputForm.jsx
@@ -1,187 +1,63 @@
 import React, { useEffect, useState } from 'react'
 import arrowChanges from './arrowChanges.svg'
-import { useSearchParams } from 'react-router-dom'
 
 /**
  * * Returns a component that allows the user to input and output values in different units.
+ * @param {String} inputUnit - The input unit of the component.
+ * @param {String} outputUnit - The output unit of the component.
+ * @param {Integer} fontSizeRoot - The output unit of the component.
+ * @param {Function} handleInputOrFontSizeValue - Function that handles the input or font size value.
+ * @param {Function} handleSwitchValueUnits - Function that handles the switch value units.
  * @returns {JSX.Element} - A component that allows the user to input and output values in different units
  */
-const InputOutputForm = () => {
+const InputOutputForm = ({
+    fontSizeRoot,
+    inputValue,
+    valueUnits: { inputUnit, outputUnit },
+    handleInputOrFontSizeValue,
+    handleSwitchValueUnits
+}) => {
     /**
-     * * Params URL
-     * @type {object} searchParams - searchParams contains functions to manipulate or get information from the URL.
-     * @type {function} setSearchParams - setSearchParams is a function that allows the user to change the URL.
-     */
-    const [searchParams, setSearchParams] = useSearchParams()
-
-    /**
-     * * The value of the input field.
-     * @type {array}
-     * @property {string} inputUnits - The value unit of the input field.
-     * @property {string} outputUnits - The value unit of the output field.
-     */
-    const [valueUnitsData, setValueUnitsData] = useState({})
-
-    /**
-     * * The value of the input field.
-     * @type {string} inputValue - The value of the input field.
-     * @type {function} setInputValue - The function that sets the value of the input field.
-     */
-    const [inputValue, setInputValue] = useState('')
-
-    /**
-     * * The value of the input field.
-     * @type {string} outputValue - The value of the output field.
-     * @type {function} setOutputValue - The function that sets the value of the output field.
+     * * Initialize output state of component.
+     * @type {Number} outputValue - represents the output value of the component.
+     * @type {Function} setOutputValue - sets the output value of the component.
      */
     const [outputValue, setOutputValue] = useState('')
 
     /**
-     * * The value of the font size root.
-     * @type {string} fontSizeRoot - The value of the font size root.
-     * @type {function} setFontSizeRoot - The function that sets the value of the font size root.
+     * * This function handle changes of the input value and font size root of the component.
+     * @param {Object} e - The event that triggered the function.
      */
-    const [fontSizeRoot, setFontSizeRoot] = useState('')
-
-    /**
-     * * Function to handle the change of the input field.
-     * @type {function} handleChangesValue - Function to handle the change of the input field.
-     * @param {string} value - The value of the input field.
-     */
-    const handleChangesValue = (e) => {
-        /**
-         * @type {string} id - The id of the input field.
-         * @type {string} value - The value of the input field.
-         */
-        const { id, value } = e.target
-
-        /**
-         * * Sets the value of the input field.
-         * * If the id is 'inputValue', sets the value of the input field, else sets the fontSizeRoot value.
-         * * If the id is 'InputValue', do check if the value is a number and dot.
-         */
-        id === 'input'
-            ? setInputValue([...value].filter((char) => /^[0-9.]$/.test(char)).join(''))
-            : setFontSizeRoot(value)
+    const handleChangesInputOrFontSizeValue = (e) => {
+        let { id, value } = e.target
+        value = [...value].filter((char) => /^[0-9.]$/.test(char)).join('')
+        handleInputOrFontSizeValue(value, id)
     }
 
     /**
-     * * Function to handle the change of the input, output, and value unit fields.
-     * @type {function} handleChangesValueUnits - Function to handle the change of the input, output, and value unit fields.
+     * * This function to handle switch the value units
+     * * This function will be called when the user clicks on the arrow changes.
+     * * When this function called, the value units will be switched with call the handleSwitchValueUnits property.
      */
-    const handleChangesUnits = () => {
-        /**
-         * * Destructuring the valueUnitsData object to get inputUnit and outputUnit value.
-         * @type {string} inputUnit - The value unit of the input field.
-         * @type {string} outputUnit - The value unit of the output field.
-         */
-        const { inputUnit, outputUnit } = valueUnitsData
-
-        /**
-         * * Sets the inputUnit and outputUnit value.
-         */
-        setValueUnitsData({
-            inputUnit: outputUnit,
-            outputUnit: inputUnit
-        })
-
-        /**
-         * * Sets the inputValue and outputValue value.
-         */
-        setInputValue(outputValue)
-        setOutputValue(inputValue)
+    const handleChangesSwitchUnits = () => {
+        handleSwitchValueUnits(inputUnit, outputUnit)
     }
 
     /**
-     * * Hooks to get the value of the input, output, and value unit fields when first opening the section.
-     * @type {function} useEffect - Hooks to get the value of the input, output, and value unit fields when first opening the section.
+     * * This hook is used to update the output value of the component.
+     * * Called when the input value, font size root, input unit and output unit changes.
      */
-    useEffect(() => {
-        /**
-         * * Get the font size root from the url with key 'font-size-root'.
-         */
-        const fontSizeRootUrl = searchParams.get('font-size-root')
-
-        /**
-         * * Get the input value unit from the url with key 'input-unit'.
-         */
-        const inputUnit = searchParams.get('input-unit')
-
-        /**
-         * * Get the output value unit from the url with key 'output-unit'.
-         */
-        const outputUnit = searchParams.get('output-unit')
-
-        /**
-         *
-         * @param {string} value - The unit value
-         * @param {*} compareValue  - The unit value to compare
-         * @param {*} code - The unit code to set (inputUnit or outputUnit)
-         * @returns The unit value to set for the input or output unit.
-         */
-        const checkInputOutputUnits = (value, compareValue, code) => {
-            if (code === 'inputUnit') {
-                if (
-                    (/[rem | em ]/gi.test(value) && /[rem | em ]/gi.test(compareValue)) ||
-                    compareValue === value
-                ) {
-                    return 'rem'
-                }
-                return value && /[rem | em | px]/gi.test(value) ? value : 'rem'
-            } else {
-                if (
-                    (/[rem | em ]/gi.test(compareValue) && /[rem | em ]/gi.test(value)) ||
-                    value === compareValue
-                )
-                    return 'px'
-                return value && /[rem | em | px]/gi.test(value) ? value : 'px'
-            }
-        }
-
-        // * Sets the inputValue value (default 16px).
-        setFontSizeRoot(fontSizeRootUrl ?? 16)
-
-        // * Sets the inputUnit value.
-        setValueUnitsData({
-            inputUnit: checkInputOutputUnits(inputUnit, outputUnit, 'inputUnit'),
-            outputUnit: checkInputOutputUnits(outputUnit, inputUnit, 'outputUnit')
-        })
-
-        // * Sets the inputValue value. If inputUnit is px set the inputValue 16px, else set the inputValue 1.
-        setInputValue(inputUnit === 'px' ? 16 : 1)
-
-        // * Sets the output value.
-        setOutputValue((value) => {
-            return value
-                ? /[rem | em]/gi.test(inputUnit) && outputUnit === 'px'
-                    ? inputValue * fontSizeRoot
-                    : inputValue / fontSizeRoot
-                : 16
-        })
-    }, [])
-
-    // changes the url when the input value of state is changed
-    useEffect(() => {
-        const { inputUnit, outputUnit } = valueUnitsData
-        setSearchParams({
-            'input-unit': inputUnit,
-            'output-unit': outputUnit,
-            'font-size-root': fontSizeRoot
-        })
-    }, [valueUnitsData, fontSizeRoot])
-
     useEffect(() => {
         const handler = setTimeout(() => {
-            const { inputUnit, outputUnit } = valueUnitsData
-
             setOutputValue(() => {
-                return /[rem | em]/gi.test(inputUnit) && outputUnit === 'px'
-                    ? inputValue * fontSizeRoot
-                    : inputValue / fontSizeRoot
+                return /[rem | em]/gi.test(inputUnit.toLowerCase()) &&
+                    outputUnit.toLowerCase() === 'px'
+                    ? Number(Number(inputValue * fontSizeRoot).toFixed(3))
+                    : Number(Number(inputValue / fontSizeRoot).toFixed(3))
             })
-        }, 350)
+        }, 150)
         return () => clearTimeout(handler)
-    }, [inputValue, fontSizeRoot, valueUnitsData])
+    }, [inputValue, fontSizeRoot, inputUnit, outputUnit])
 
     return (
         <div className="flex flex-col items-center justify-center">
@@ -192,13 +68,13 @@ const InputOutputForm = () => {
                         className=" max-w-[150px] appearance-none border-0 border-b-2 border-white-100 bg-transparent p-2 text-center font-poppins text-[2.5rem] font-bold text-white-100 placeholder:text-xs placeholder:font-thin placeholder:text-white-65 focus:outline-none"
                         value={inputValue}
                         id="input"
-                        onChange={handleChangesValue}
+                        onChange={handleChangesInputOrFontSizeValue}
                         placeholder="Enter a value"
                     />
                     <label
                         htmlFor="input"
                         className="m-2 text-center font-inter text-[2rem] text-white-80">
-                        {valueUnitsData.inputUnit}
+                        {inputUnit}
                     </label>
                 </form>
 
@@ -206,7 +82,7 @@ const InputOutputForm = () => {
                     src={arrowChanges}
                     className="m-auto w-[48px] cursor-pointer"
                     alt="Arrow Changes Image"
-                    onClick={handleChangesUnits}
+                    onClick={handleChangesSwitchUnits}
                 />
 
                 <div className="mt-8 mb-4 flex flex-col lg:m-12">
@@ -220,16 +96,16 @@ const InputOutputForm = () => {
                     <label
                         htmlFor="input"
                         className="text32xl m-2 text-center font-inter text-[2rem] text-white-65">
-                        {valueUnitsData.outputUnit}
+                        {outputUnit}
                     </label>
                 </div>
             </div>
             <p className=" text-sm text-white-80 sm:text-base">
-                Calculation based root font size{' '}
+                Calculation based font size root{' '}
                 <input
                     type="text"
                     value={fontSizeRoot}
-                    onChange={handleChangesValue}
+                    onChange={handleChangesInputOrFontSizeValue}
                     className="m-1 max-w-[40px] border-[1px] bg-transparent text-center"
                     id="font-size-root"
                 />{' '}
