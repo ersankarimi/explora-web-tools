f02135454d171b1a0b46442eed9561505fe25f9c
Ersan Karimi <73420137+ersankarimi@users.noreply.github.com>
2022-07-14 14:05:43 +0800

    Update : reformat line

--- src/App.jsx
+++ src/App.jsx
@@ -8,84 +8,82 @@ import { useSidebar } from '@hooks'
  * @returns {Any} App - the main component.
  */
 const App = () => {
-    /**
-     * @constant
-     * @type {Boolean} represents the state of the sidebar
-     * @default true when the viewport is greater than 639px
-     *
-     * @constant
-     * @type {Function} represents the function to toggle the sidebar
-     * @default useSidebar(SidebarContext)
-     */
-    const [sidebarIsOpen, toggleSidebar] = useSidebar()
+  /**
+   * @constant
+   * @type {Boolean} represents the state of the sidebar
+   * @default true when the viewport is greater than 639px
+   *
+   * @constant
+   * @type {Function} represents the function to toggle the sidebar
+   * @default useSidebar(SidebarContext)
+   */
+  const [sidebarIsOpen, toggleSidebar] = useSidebar()
 
-    /**
-     * * Returns a toggleSedebar function,
-     * * if the innerWidth is <= 639px and,
-     * *  target event doesn't have the className 'sidebar__dropdown-button' and 'arrow-button4.
-     */
-    const handleWindowResize = () => {
-        const { innerWidth } = window
-        innerWidth > 767 && !sidebarIsOpen && toggleSidebar()
-        innerWidth <= 767 && sidebarIsOpen && toggleSidebar()
+  /**
+   * * Returns a toggleSedebar function,
+   * * if the innerWidth is <= 639px and,
+   * *  target event doesn't have the className 'sidebar__dropdown-button' and 'arrow-button4.
+   */
+  const handleWindowResize = () => {
+    const { innerWidth } = window
+    innerWidth > 767 && !sidebarIsOpen && toggleSidebar()
+    innerWidth <= 767 && sidebarIsOpen && toggleSidebar()
+  }
+
+  /**
+   * * Adds the event listener to the window resize event
+   * * and calls the handleWindowResize function
+   * * when the component is mounted.
+   */
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowResize)
+    return () => {
+      window.removeEventListener('resize', handleWindowResize)
     }
+  }, [window.innerWidth])
 
-    /**
-     * * Adds the event listener to the window resize event
-     * * and calls the handleWindowResize function
-     * * when the component is mounted.
-     */
-    useEffect(() => {
-        window.addEventListener('resize', handleWindowResize)
-        return () => {
-            window.removeEventListener('resize', handleWindowResize)
-        }
-    }, [window.innerWidth])
+  let { pathname } = useLocation()
+  pathname = pathname.split('/')[1]
 
-    let { pathname } = useLocation()
-    pathname = pathname.split('/')[1]
+  /**
+   * * Check the path name and return the appropriate component.
+   * * Changes the title of the page.
+   */
+  useEffect(() => {
+    pathname === 'css'
+      ? (document.title = 'CSS Tools | Explora web tools, The helper tools for web development')
+      : pathname === 'html'
+      ? (document.title = 'HTML Tools | Explora web tools, The helper tools for web development')
+      : (document.title = 'Explora Web Tools | The helper tools for web development')
+  }, [pathname])
 
-    /**
-     * * Check the path name and return the appropriate component.
-     * * Changes the title of the page.
-     */
-    useEffect(() => {
-        pathname === 'css'
-            ? (document.title =
-                  'CSS Tools | Explora web tools, The helper tools for web development')
-            : pathname === 'html'
-            ? (document.title =
-                  'HTML Tools | Explora web tools, The helper tools for web development')
-            : (document.title = 'Explora Web Tools | The helper tools for web development')
-    }, [pathname])
-
-    return (
-        <>
-            <motion.aside
-                className="container__sidebar sticky top-0 z-50 h-screen min-w-[50%] max-w-[50%] md:min-w-[30%] md:max-w-[30%] lg:min-w-[15%] lg:max-w-[15%]"
-                animate={{
-                    x: sidebarIsOpen ? 0 : -400,
-                    display: sidebarIsOpen ? 'block' : 'none'
-                }}
-                transition={{
-                    duration: 0.25,
-                    type: 'spring'
-                }}>
-                <Sidebar />
-            </motion.aside>
-            <div className="container__content flex min-h-screen flex-col">
-                <header className="sticky top-0 z-30">
-                    <Navbar />
-                </header>
-                <main className="relative h-full overflow-y-auto p-4 lg:py-4 lg:px-12">
-                    <Outlet />
-                </main>
-                <footer>
-                    <Footer />
-                </footer>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <motion.aside
+        className="container__sidebar sticky top-0 z-50 h-screen min-w-[50%] max-w-[50%] md:min-w-[30%] md:max-w-[30%] lg:min-w-[15%] lg:max-w-[15%]"
+        animate={{
+          x: sidebarIsOpen ? 0 : -400,
+          display: sidebarIsOpen ? 'block' : 'none'
+        }}
+        transition={{
+          duration: 0.25,
+          type: 'spring'
+        }}>
+        <Sidebar />
+      </motion.aside>
+      <div className="container__content flex min-h-screen flex-col">
+        <header className="sticky top-0 z-30">
+          <Navbar />
+        </header>
+        <main className="relative h-full overflow-y-auto p-4 lg:py-4 lg:px-12">
+          <Outlet />
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
+    </>
+  )
 }
 
 export default App
--- src/features/Content/Css/RelativeLengthUnits/components/InputOutputForm/InputOutputForm.jsx
+++ src/features/Content/Css/RelativeLengthUnits/components/InputOutputForm/InputOutputForm.jsx
@@ -11,108 +11,105 @@ import arrowChanges from './arrowChanges.svg'
  * @returns {JSX.Element} - A component that allows the user to input and output values in different units
  */
 const InputOutputForm = ({
-    fontSizeRoot,
-    inputValue,
-    valueUnits: { inputUnit, outputUnit },
-    handleInputOrFontSizeValue,
-    handleSwitchValueUnits
+  fontSizeRoot,
+  inputValue,
+  valueUnits: { inputUnit, outputUnit },
+  handleInputOrFontSizeValue,
+  handleSwitchValueUnits
 }) => {
-    /**
-     * * Initialize output state of component.
-     * @type {Number} outputValue - represents the output value of the component.
-     * @type {Function} setOutputValue - sets the output value of the component.
-     */
-    const [outputValue, setOutputValue] = useState('')
+  /**
+   * * Initialize output state of component.
+   * @type {Number} outputValue - represents the output value of the component.
+   * @type {Function} setOutputValue - sets the output value of the component.
+   */
+  const [outputValue, setOutputValue] = useState('')
 
-    /**
-     * * This function handle changes of the input value and font size root of the component.
-     * @param {Object} e - The event that triggered the function.
-     */
-    const handleChangesInputOrFontSizeValue = (e) => {
-        let { id, value } = e.target
-        value = [...value].filter((char) => /^[0-9.]$/.test(char)).join('')
-        handleInputOrFontSizeValue(value, id)
-    }
+  /**
+   * * This function handle changes of the input value and font size root of the component.
+   * @param {Object} e - The event that triggered the function.
+   */
+  const handleChangesInputOrFontSizeValue = (e) => {
+    let { id, value } = e.target
+    value = [...value].filter((char) => /^[0-9.]$/.test(char)).join('')
+    handleInputOrFontSizeValue(value, id)
+  }
 
-    /**
-     * * This function to handle switch the value units
-     * * This function will be called when the user clicks on the arrow changes.
-     * * When this function called, the value units will be switched with call the handleSwitchValueUnits property.
-     */
-    const handleChangesSwitchUnits = () => {
-        handleSwitchValueUnits(inputUnit, outputUnit)
-    }
+  /**
+   * * This function to handle switch the value units
+   * * This function will be called when the user clicks on the arrow changes.
+   * * When this function called, the value units will be switched with call the handleSwitchValueUnits property.
+   */
+  const handleChangesSwitchUnits = () => {
+    handleSwitchValueUnits(inputUnit, outputUnit)
+  }
 
-    /**
-     * * This hook is used to update the output value of the component.
-     * * Called when the input value, font size root, input unit and output unit changes.
-     */
-    useEffect(() => {
-        const handler = setTimeout(() => {
-            setOutputValue(() => {
-                return /[rem | em]/gi.test(inputUnit.toLowerCase()) &&
-                    outputUnit.toLowerCase() === 'px'
-                    ? Number(Number(inputValue * fontSizeRoot).toFixed(3))
-                    : Number(Number(inputValue / fontSizeRoot).toFixed(3))
-            })
-        }, 150)
-        return () => clearTimeout(handler)
-    }, [inputValue, fontSizeRoot, inputUnit, outputUnit])
+  /**
+   * * This hook is used to update the output value of the component.
+   * * Called when the input value, font size root, input unit and output unit changes.
+   */
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setOutputValue(() => {
+        return /[rem | em]/gi.test(inputUnit.toLowerCase()) && outputUnit.toLowerCase() === 'px'
+          ? Number(Number(inputValue * fontSizeRoot).toFixed(3))
+          : Number(Number(inputValue / fontSizeRoot).toFixed(3))
+      })
+    }, 150)
+    return () => clearTimeout(handler)
+  }, [inputValue, fontSizeRoot, inputUnit, outputUnit])
 
-    return (
-        <div className="flex flex-col items-center justify-center">
-            <div className="flex flex-col lg:flex-row">
-                <form className="mb-8 mt-4 flex flex-col lg:m-12">
-                    <input
-                        type="text"
-                        className=" max-w-[150px] appearance-none border-0 border-b-2 border-white-100 bg-transparent p-2 text-center font-poppins text-[2.5rem] font-bold text-white-100 placeholder:text-xs placeholder:font-thin placeholder:text-white-65 focus:outline-none"
-                        value={inputValue}
-                        id="input"
-                        onChange={handleChangesInputOrFontSizeValue}
-                        placeholder="Enter a value"
-                    />
-                    <label
-                        htmlFor="input"
-                        className="m-2 text-center font-inter text-[2rem] text-white-80">
-                        {inputUnit}
-                    </label>
-                </form>
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col lg:flex-row">
+        <form className="mb-8 mt-4 flex flex-col lg:m-12">
+          <input
+            type="text"
+            className=" max-w-[150px] appearance-none border-0 border-b-2 border-white-100 bg-transparent p-2 text-center font-poppins text-[2.5rem] font-bold text-white-100 placeholder:text-xs placeholder:font-thin placeholder:text-white-65 focus:outline-none"
+            value={inputValue}
+            id="input"
+            onChange={handleChangesInputOrFontSizeValue}
+            placeholder="Enter a value"
+          />
+          <label htmlFor="input" className="m-2 text-center font-inter text-[2rem] text-white-80">
+            {inputUnit}
+          </label>
+        </form>
 
-                <img
-                    src={arrowChanges}
-                    className="m-auto w-[48px] cursor-pointer"
-                    alt="Arrow Changes Image"
-                    onClick={handleChangesSwitchUnits}
-                />
+        <img
+          src={arrowChanges}
+          className="m-auto w-[48px] cursor-pointer"
+          alt="Arrow Changes Image"
+          onClick={handleChangesSwitchUnits}
+        />
 
-                <div className="mt-8 mb-4 flex flex-col lg:m-12">
-                    <input
-                        type="text"
-                        className="max-w-[150px] border-b-2 border-white-80 bg-transparent pb-2 text-center font-poppins text-[2.5rem] font-bold text-white-80"
-                        value={outputValue}
-                        disabled={true}
-                        id="output"
-                    />
-                    <label
-                        htmlFor="input"
-                        className="text32xl m-2 text-center font-inter text-[2rem] text-white-65">
-                        {outputUnit}
-                    </label>
-                </div>
-            </div>
-            <p className=" text-sm text-white-80 sm:text-base">
-                Calculation based font size root{' '}
-                <input
-                    type="text"
-                    value={fontSizeRoot}
-                    onChange={handleChangesInputOrFontSizeValue}
-                    className="m-1 max-w-[40px] border-[1px] bg-transparent text-center"
-                    id="font-size-root"
-                />{' '}
-                pixel
-            </p>
+        <div className="mt-8 mb-4 flex flex-col lg:m-12">
+          <input
+            type="text"
+            className="max-w-[150px] border-b-2 border-white-80 bg-transparent pb-2 text-center font-poppins text-[2.5rem] font-bold text-white-80"
+            value={outputValue}
+            disabled={true}
+            id="output"
+          />
+          <label
+            htmlFor="input"
+            className="text32xl m-2 text-center font-inter text-[2rem] text-white-65">
+            {outputUnit}
+          </label>
         </div>
-    )
+      </div>
+      <p className=" text-sm text-white-80 sm:text-base">
+        Calculation based font size root{' '}
+        <input
+          type="text"
+          value={fontSizeRoot}
+          onChange={handleChangesInputOrFontSizeValue}
+          className="m-1 max-w-[40px] border-[1px] bg-transparent text-center"
+          id="font-size-root"
+        />{' '}
+        pixel
+      </p>
+    </div>
+  )
 }
 
 export default InputOutputForm
