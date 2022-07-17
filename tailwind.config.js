43a07a29e920d64648834182496b9e793f7121d8
Ersan Karimi <73420137+ersankarimi@users.noreply.github.com>
2022-07-17 13:47:58 +0800

    Update : memperbarui tailwind.config.js
    - Memperbarui tabWidth indent dari 4 menjadi 2
    - Menambahkan warna baru pada object colors yaitu "button-dark-light"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,51 +1,54 @@
 module.exports = {
-    content: ['index.html', './src/**/*.{js,jsx,ts,tsx,vue,html}'],
-    theme: {
-        extend: {
-            screens: {
-                '3xl': '2560px'
-            },
-            colors: {
-                // * Neutral
-                'white-100': '#FAFAFA',
-                'white-80': 'rgba(250,250,250,.8)',
-                'white-65': 'rgba(250,250,250,.65)',
+  content: ['index.html', './src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  theme: {
+    extend: {
+      screens: {
+        '3xl': '2560px'
+      },
+      colors: {
+        // * Neutral
+        'white-100': '#FAFAFA',
+        'white-80': 'rgba(250,250,250,.8)',
+        'white-65': 'rgba(250,250,250,.65)',
 
-                // * Primary
-                'main-bg-dark-1': '#181C27',
-                'main-bg-dark-2': '#394253',
-                'main-card-dark-1': '#2C3341',
-                'main-card-dark-2': '#313949',
-                'main-sidebar-dark': '#313949',
+        // * Primary
+        'main-bg-dark-1': '#181C27',
+        'main-bg-dark-2': '#394253',
+        'main-card-dark-1': '#2C3341',
+        'main-card-dark-2': '#313949',
+        'main-sidebar-dark': '#313949',
 
-                // * Table Colors
-                'table-color-1': '#313949',
-                'table-color-2': '#374151',
+        // * Table Colors
+        'table-color-1': '#313949',
+        'table-color-2': '#374151',
 
-                // * Accent
-                'purple-accent': '#5219CF',
-                'coral-accent': '#FFAB91',
+        // * Accent
+        'purple-accent': '#5219CF',
+        'coral-accent': '#FFAB91',
 
-                // * Stroke
-                'item-stroke': 'rgba(49,57,73,0.5)'
-            },
-            boxShadow: {
-                'item-shadow': ' 10px 10px 20px #262C37, -10px -10px 20px rgba(72, 80, 99, 0.26)',
-                'sidebar-shadow':
-                    '-10px -10px 20px rgba(57, 66, 83, 0.35), 10px 10px 20px rgba(24, 28, 39, 0.35)',
-                'hover-dropdown-shadow':
-                    '-10px -10px 20px rgba(57, 66, 83, 0.35), 10px 10px 20px rgba(24, 28, 39, 0.35)',
-                'active-dropdown-shadow':
-                    '5px -5px 10px rgba(72, 80, 99, 0.1), 5px 5px 10px rgba(24, 28, 39, 0.35), inset -2px -2px 4px rgba(72, 80, 99, 0.35), inset 5px 5px 5px #1D222E;',
-                'header-shadow':
-                    '-10px -10px 20px rgba(57, 66, 83, 0.35) 10px 10px 20px rgba(24, 28, 39, 0.35)',
-                'table-header-shadow': '0px 4px 8px rgba(0, 0, 0, 0.16)'
-            },
-            fontFamily: {
-                poppins: ['Poppins', 'sans-serif'],
-                inter: ['Inter', 'sans-serif']
-            }
-        }
-    },
-    plugins: []
+        // * Stroke
+        'item-stroke': 'rgba(49,57,73,0.5)',
+
+        // button color
+        'button-dark-light': '#525F7A'
+      },
+      boxShadow: {
+        'item-shadow': ' 10px 10px 20px #262C37, -10px -10px 20px rgba(72, 80, 99, 0.26)',
+        'sidebar-shadow':
+          '-10px -10px 20px rgba(57, 66, 83, 0.35), 10px 10px 20px rgba(24, 28, 39, 0.35)',
+        'hover-dropdown-shadow':
+          '-10px -10px 20px rgba(57, 66, 83, 0.35), 10px 10px 20px rgba(24, 28, 39, 0.35)',
+        'active-dropdown-shadow':
+          '5px -5px 10px rgba(72, 80, 99, 0.1), 5px 5px 10px rgba(24, 28, 39, 0.35), inset -2px -2px 4px rgba(72, 80, 99, 0.35), inset 5px 5px 5px #1D222E;',
+        'header-shadow':
+          '-10px -10px 20px rgba(57, 66, 83, 0.35) 10px 10px 20px rgba(24, 28, 39, 0.35)',
+        'table-header-shadow': '0px 4px 8px rgba(0, 0, 0, 0.16)'
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        inter: ['Inter', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
 }
