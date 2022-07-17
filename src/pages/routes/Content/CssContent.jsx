83d96d77ccacd4c787946b699324207880113e23
Ersan Karimi <73420137+ersankarimi@users.noreply.github.com>
2022-07-17 13:44:36 +0800

    Update : memperbarui CssContent.jsx
    - Mengimport komponen CssBoxShadow dari @features/Content
    - Menambah case baru yaitu "cssboxshadow", saat pathname cocok dengan case ini maka akan membuka component CssBoxShadow

--- src/pages/routes/Content/CssContent.jsx
+++ src/pages/routes/Content/CssContent.jsx
@@ -1,24 +1,26 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import { UnderConstructions } from '@features/ui'
-import { RelativeLengthUnits } from '@features/Content'
+import { RelativeLengthUnits, CssBoxShadow } from '@features/Content'
 
 /**
  * * Returns a component that renders the content of the section.
  * @returns {JSX.Element} CssContent - JSX Element of the CSS Content Routes .
  */
 const CssContent = () => {
-    /**
-     * @constant
-     * @type {String} section - the current section pathname from the url params object
-     */
-    const { section } = useParams()
-    switch (section) {
-        case 'relativelengthunits':
-            return <RelativeLengthUnits />
-        default:
-            return <UnderConstructions />
-    }
+  /**
+   * @constant
+   * @type {String} section - the current section pathname from the url params object
+   */
+  const { section } = useParams()
+  switch (section) {
+    case 'relativelengthunits':
+      return <RelativeLengthUnits />
+    case 'cssboxshadow':
+      return <CssBoxShadow />
+    default:
+      return <UnderConstructions />
+  }
 }
 
 export default CssContent
