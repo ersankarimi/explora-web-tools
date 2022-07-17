e31de6ef14cc8f1f5f5f988d5f4a7fca6dd0cd3e
Ersan Karimi <73420137+ersankarimi@users.noreply.github.com>
2022-07-17 13:54:25 +0800

    Make a new component : membuat komponen baru dan file untuk aturan animasi komponen tersebut
    - Membuat colorPickerAnimation.js yang mana akan meng-export variabel colorPickerVariants yang berisi aturan animasi komponen tersebut
    - Membuat komponen ColorPicker yang akan meng-export komponen yang berguna sebagai memilih warna. Komponen ini menggunakan <ChromePicker/> dari library react-color

---
+++ src/features/Content/Css/CssBoxShadow/components/Options/components/DropdownBoxShadow/components/DropdownItem/components/ColorPicker/ColorPicker.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import { motion } from 'framer-motion'
+import { ChromePicker } from 'react-color'
+import { colorPickerVariants } from './colorPickerAnimation'
+
+const ColorPicker = ({ name, optionValue, id, handleChangeValue }) => {
+  const [colorIsOpen, setColorIsOpen] = useState(false)
+
+  const handleColorIsOpen = () => {
+    setColorIsOpen(!colorIsOpen)
+  }
+
+  return (
+    <>
+      <motion.div className=" relative my-2 flex w-full flex-col items-start justify-start">
+        <label
+          htmlFor={`${name}${id}`}
+          className="font-inter text-sm font-normal italic text-white-65">
+          {name}
+        </label>
+        <div className="mt-1 flex w-full">
+          <input
+            type="text"
+            id={`${name}${id}`}
+            value={optionValue}
+            onChange={(e) => handleChangeValue(e.target.value, id, name)}
+            className="appearance-none rounded-md border-2 border-white-80 bg-table-color-2 px-2 py-1 text-white-80 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-white-80"
+          />
+          <div
+            className="mx-2 w-8 cursor-pointer rounded-md border-2 border-slate-400"
+            style={{
+              backgroundColor: optionValue
+            }}
+            onClick={handleColorIsOpen}></div>
+        </div>
+        <motion.div
+          className="w-full"
+          variants={colorVariants}
+          initial="hidden"
+          animate={colorIsOpen ? 'visible' : 'hidden'}>
+          <ChromePicker
+            color={optionValue}
+            onChange={(color, event) => {
+              const { r, g, b, a } = color.rgb
+              return handleChangeValue(
+                color.rgb.a === 1 ? color.hex : `rgba(${r}, ${g}, ${b}, ${a})`,
+                id,
+                name
+              )
+            }}
+          />
+        </motion.div>
+      </motion.div>
+    </>
+  )
+}
+
+export default ColorPicker
---
+++ src/features/Content/Css/CssBoxShadow/components/Options/components/DropdownBoxShadow/components/DropdownItem/components/ColorPicker/colorPickerAnimation.js
@@ -0,0 +1,18 @@
+export const colorPickerVariants = {
+  hidden: {
+    opacity: 0,
+    display: 'none',
+    scale: 0,
+    transition: {
+      duration: 0.5
+    }
+  },
+  visible: {
+    opacity: 1,
+    display: 'block',
+    scale: 1,
+    transition: {
+      duration: 0.5
+    }
+  }
+}
